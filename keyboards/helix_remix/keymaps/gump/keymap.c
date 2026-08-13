d18a8035d9c76158f8c40535f62b008c9b9773b9
Forrest Nichols <forrestdn123@gmail.com>
2026-08-13 12:04:07 -0600

    Attempt at fixing errors #2

--- keyboards/helix_remix/keymaps/gump/keymap.c
+++ keyboards/helix_remix/keymaps/gump/keymap.c
@@ -11,21 +11,15 @@
 // #include "keymap_japanese.h"
 enum layer_number {
     _BASE = 0,
-    _FUNC,
-    _VIA1,
-    _VIA2,
+    _LOWER,
+    _RAISE,
+    _ADJUST,
 };
 
 
 enum custom_keycodes {
   MAC_WIN = SAFE_RANGE,
 };
-
-#define DL_BAS  DF(_BASE)
-#define ML_FUNC  MO(_LOWER)
-#define ML_VIA1  MO(_RAISE)
-#define ML_VIA2  MO(_ADJUST)
-
 
 // keymapviz keymap.c -k helix -t json -o hremix{}.json
 
@@ -35,16 +29,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,                   KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,   \
     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,   KC_G,                   KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT, \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,   KC_ENT, KC_DEL, KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_RSFT,  \
-    KC_LCTL, KC_LALT, KC_LGUI, ML_CLIP, ML_UPP, ML_LOW, KC_SPC, KC_SPC, ML_LOW, ML_CLIP, KC_PSCR, KC_LEFT, KC_DOWN, KC_RGHT  \
+    KC_LCTL, KC_LALT, KC_LGUI, _______, _______, _______, KC_SPC, KC_SPC, _______, _______, KC_PSCR, KC_LEFT, KC_DOWN, KC_RGHT  \
     ),
 
   [_LOWER] = KEY_LAYOUT( \
-    S(KC_SCLN), KC_MINS,   _______, KC_SLSH,    S(KC_5),    S(KC_MINS),                   JP_CIRC,    JP_YEN,     KC_PEQL, KC_PSLS,  KC_PAST, KC_PMNS, \
-    S(JP_YEN),  S(KC_6),   S(KC_1),    S(KC_SLSH), JP_AT,      JP_YEN,                       JP_AT,      JP_LBRC,    KC_P7,   KC_P8,    KC_P9,   KC_BSPC,  \
-    S(KC_8),    S(KC_9),   JP_LBRC,    JP_RBRC,    S(JP_LBRC), S(JP_RBRC),                   JP_COLN,    JP_RBRC,    KC_P4,   KC_P5,    KC_P6,   KC_PENT, \
-    S(KC_COMM), S(KC_DOT), S(JP_BSLS), JP_CIRC,    S(JP_AT),   S(JP_CIRC), KC_SLEP, KC_SLEP, KC_SLSH,    JP_BSLS,    KC_P1,   KC_P2,    KC_P3,   KC_RALT, \
-    _______,    _______,   _______,    _______,    _______,    _______,    _______, _______, S(KC_SLSH), S(JP_BSLS), KC_P0,   KC_COMMA, KC_PDOT, KC_PPLS \
-    ),
+    S(KC_SCLN), KC_MINS,    _______,    KC_SLSH,    S(KC_5),    S(KC_MINS),                     _______,        _______,    KC_PEQL, KC_PSLS,  KC_PAST, KC_PMNS, \
+    _______,  S(KC_6),   S(KC_1),    S(KC_SLSH), _______,    _______,                        _______,        _______,    KC_P7,   KC_P8,    KC_P9,   KC_BSPC,  \
+    S(KC_8),    S(KC_9),    _______,    _______,    _______,    _______,                        _______,        _______,    KC_P4,   KC_P5,    KC_P6,   KC_PENT, \
+    S(KC_COMM), S(KC_DOT),  _______,    _______,    _______,    _______,    KC_SLEP, KC_SLEP,   KC_SLSH,        _______,    KC_P1,   KC_P2,    KC_P3,   KC_RALT, \
+    _______,    _______,    _______,    _______,    _______,    _______,    _______, _______,   S(KC_SLSH),     _______,    KC_P0,   KC_COMMA, KC_PDOT, KC_PPLS \
+    ),  
 
   [_RAISE] = KEY_LAYOUT( \
     KC_F1,      KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11 , KC_F12 , \
@@ -55,11 +49,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     
   [_ADJUST] =  KEY_LAYOUT( \
-    _______, _______, _______, _______, _______, _______,                     KC_KP_1,    KC_KP_2,    KC_KP_3,       KC_KP_4, KC_KP_5, _______, \
-    _______, _______, _______, _______, _______, _______,                     KC_KP_6,    KC_KP_7,    KC_KP_8,       KC_KP_9, KC_KP_0, KC_BSPC, \
-    _______, _______, _______, _______, _______, _______,                     LCA(KC_F1), LCA(KC_F2), S(G(KC_RGHT)), KC_LSFT, KC_LCTL, KC_ENT, \
-    _______, _______, _______, _______, _______, _______, _______,   _______, C(KC_X),    C(KC_C),    C(KC_V),       KC_LGUI, KC_UP,   KC_SPC, \
-    _______, _______, _______, _______, _______, _______, _______,   _______, _______,     _______,    KC_PSCR,       KC_LEFT, KC_DOWN, KC_RGHT \
+    _______, _______, _______, _______, _______, _______,                     KC_KP_1,      KC_KP_2,    KC_KP_3,       KC_KP_4, KC_KP_5, _______, \
+    _______, _______, _______, _______, _______, _______,                     KC_KP_6,      KC_KP_7,    KC_KP_8,       KC_KP_9, KC_KP_0, KC_BSPC, \
+    _______, _______, _______, _______, _______, _______,                     _______,      _______,    _______,       KC_LSFT, KC_LCTL, KC_ENT, \
+    _______, _______, _______, _______, _______, _______, _______,   _______, _______,      _______,    _______,       KC_LGUI, KC_UP,   KC_SPC, \
+    _______, _______, _______, _______, _______, _______, _______,   _______, _______,      _______,    KC_PSCR,       KC_LEFT, KC_DOWN, KC_RGHT \
     ),
 };
 
